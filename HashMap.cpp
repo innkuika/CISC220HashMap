05ef56a7dc6cc741f260d782b7207ca21542fe65
innkuika <innkuika@gmail.com>
2020-05-11 08:25:15 -0400

    debug and more debug

--- HashMap.cpp
+++ HashMap.cpp
@@ -5,14 +5,14 @@
  *      Author: 13027
  */
 
-
 #include "HashMap.hpp"
 #include "HashNode.hpp"
 #include <iostream>
 #include <math.h>
 using namespace std;
 
-hashMap::hashMap(bool hash1, bool coll1) {
+hashMap::hashMap(bool hash1, bool coll1)
+{
 	first = "";
 	numKeys = 0;
 	mapSize = 7;
@@ -24,132 +24,162 @@ hashMap::hashMap(bool hash1, bool coll1) {
 	hashNode *mapNode = new hashNode[mapSize];
 	map = &mapNode;
 
-	for(int i = 0; i < mapSize; i++){
+	for (int i = 0; i < mapSize; i++)
+	{
 		mapNode[i].values = NULL;
 	}
-
 }
-void hashMap::addKeyValue(string k, string v) {
-	int index = hashfn ? calcHash1(k) :calcHash2(k);
-
+void hashMap::addKeyValue(string k, string v)
+{
+	cout << "inside addKeyValue" << endl;
+	int index = hashfn ? calcHash1(k) : calcHash2(k);
 	hashNode *nodeAtIndex = map[index];
-	cout<<"here1"<<endl;
-	if(nodeAtIndex == NULL){
-		cout<<"here2"<<endl;
-		map[index] = new hashNode(k);
-		map[index]->addValue(v);
-	}
-	else if(nodeAtIndex->keyword == k){
-		cout<<"here3"<<endl;
-		map[index]->addValue(v);
-	}
-	else{
-		cout<<"here4"<<endl;
-		int ct = 0;
-		hashcoll++;
-		while(nodeAtIndex->keyword != k){
-			index = collfn ? coll1(index, ct, k) : coll2(index, ct, k);
-			nodeAtIndex = map[index];
-			ct++;
-			if(nodeAtIndex == NULL) break;
+	int ct = 0;
+	// if(nodeAtIndex != NULL){
+	// 	cout<<"test"<<endl;
+	// 	cout<<nodeAtIndex <<endl;
+	// 	cout<<nodeAtIndex->keyword <<endl;
+	// }
+	while (true)
+	{
+		if (nodeAtIndex == NULL)
+		{
+			cout << "here6" << endl;
+			map[index] = new hashNode(k);
+			map[index]->addValue(v);
+			break;
 		}
-		collisions += ct - 1;
+		else if (nodeAtIndex->keyword == k)
+		{
+			cout << "here7" << endl;
+			map[index]->addValue(v);
+			break;
+		}
+		if(ct == 0) hashcoll++;
+		index = collfn ? coll1(index, ct, k) : coll2(index, ct, k);
+		nodeAtIndex = map[index];
+		ct++;
 	}
-	cout<<"here5"<<endl;
+	collisions += ct - 1;
+
+	cout << "here5" << endl;
+	cout << "hash index: " << index << endl;
+	cout << "numKeys: " << numKeys<< endl;
+	cout << "mapSize: " << mapSize << endl;
 
 	numKeys++;
-	if(numKeys/mapSize > 0.7)reHash();
-
+	if (numKeys / mapSize > 0.7)
+		reHash();
+	cout << "here10" << endl;
 }
-int hashMap::getIndex(string k) {
+int hashMap::getIndex(string k)
+{
 	//pass for now
 	//will come back and implement
 	return -1099;
 }
 
-int hashMap::calcHash1(string k){
-	return ((int)k[0]+(int)k[1]*27+(int)k[2]*27*27) % mapSize;		
+int hashMap::calcHash1(string k)
+{
+	return ((int)k[0] + (int)k[1] * 27 + (int)k[2] * 27 * 27) % mapSize;
 }
 
-int hashMap::calcHash2(string k){
+int hashMap::calcHash2(string k)
+{
 	int h;
 	int L = k.length();
-	for (int j = 0; j < L; j++) {
-			h += (int)k[j];
-		}
+	for (int j = 0; j < L; j++)
+	{
+		h += (int)k[j];
+	}
 	return h % mapSize;
 }
 
-bool isPrime(int n)  
-{   
-    if (n == 1)  return false; 
+bool isPrime(int n)
+{
+	if (n == 1)
+		return false;
 
-    for (int i=2; i < n; i++)  
-        if (n%i == 0)  
-           return false;  
+	for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			return false;
 
-    return true;  
-}  
-void hashMap::getClosestPrime() {
-    while (true) { 
-        mapSize++; 
-        if (isPrime(mapSize)) break;
-    } 
-
+	return true;
 }
-void hashMap::reHash() {
-	cout<<"inside rehash"<<endl;
+void hashMap::getClosestPrime()
+{
+	while (true)
+	{
+		mapSize++;
+		if (isPrime(mapSize))
+			break;
+	}
+}
+void hashMap::reHash()
+{
+	cout << "inside rehash" << endl;
 	int oldMapSize = mapSize;
 	hashNode **oldMap = map;
-	cout<<"map size:"<<mapSize<<endl;
+	cout << "map size:" << mapSize << endl;
 	getClosestPrime();
-	cout<<"map size:"<<mapSize<<endl;
+	numKeys = 0;
+	cout << "map size:" << mapSize << endl;
 	hashNode *newMap = new hashNode[mapSize];
-	for(int i = 0; i < mapSize; i++){
+	for (int i = 0; i < mapSize; i++)
+	{
 		newMap[i].values = NULL;
 	}
-	map = & newMap;
-	for(int i = 0; i < oldMapSize; i++){
-		if(oldMap[i] != NULL){
-		for(int j = 0; j < oldMap[i]->currSize; j++){
-			cout<<"test2"<<endl;
-			addKeyValue(oldMap[i]->keyword, oldMap[i]->values[j]);
-		}
+	map = &newMap;
+	for (int i = 0; i < oldMapSize; i++)
+	{
+		if (oldMap[i] != NULL)
+		{
+			for (int j = 0; j < oldMap[i]->currSize; j++)
+			{
+				cout << "test2" << endl;
+				addKeyValue(oldMap[i]->keyword, oldMap[i]->values[j]);
+			}
 		}
 	}
 }
-int hashMap::coll1(int h, int i, string k) {
-	cout<<"inside coll1"<<endl;
-	return (h + i)%mapSize;
+int hashMap::coll1(int h, int i, string k)
+{
+	cout << "inside coll1" << endl;
+	return (h + i) % mapSize;
 }
-int hashMap::coll2(int h, int i, string k) {
-	cout<<"inside coll2"<<endl;
-	return (h + i*i)%mapSize;
+int hashMap::coll2(int h, int i, string k)
+{
+	cout << "inside coll2" << endl;
+	return (h + i * i) % mapSize;
 }
-int hashMap::findKey(string k) {
-//NOTE: THIS METHOD CANNOT LOOP from index 0 to end of hash array looking for the key.  That destroys any efficiency in run-time. 
-int index = hashfn? calcHash1(k) : calcHash2(k);
-int ct = 0;
-while(map[index]->keyword != k){
-	if(map[index] == NULL) return -1;
-	index = collfn ? coll1(index, ct, k) : coll2(index, ct, k);
-}
-return index;
+int hashMap::findKey(string k)
+{
+	//NOTE: THIS METHOD CANNOT LOOP from index 0 to end of hash array looking for the key.  That destroys any efficiency in run-time.
+	int index = hashfn ? calcHash1(k) : calcHash2(k);
+	int ct = 0;
+	while (map[index]->keyword != k)
+	{
+		if (map[index] == NULL)
+			return -1;
+		index = collfn ? coll1(index, ct, k) : coll2(index, ct, k);
+	}
+	return index;
 }
 
-
-void hashMap::printMap() {
+void hashMap::printMap()
+{
 	cout << "In printMap()" << endl;
-	for (int i = 0; i < mapSize; i++) {
+	for (int i = 0; i < mapSize; i++)
+	{
 		//cout << "In loop" << endl;
-		if (map[i] != NULL) {
+		if (map[i] != NULL)
+		{
 			cout << map[i]->keyword << ": ";
-			for (int j = 0; j < map[i]->currSize;j++) {
+			for (int j = 0; j < map[i]->currSize; j++)
+			{
 				cout << map[i]->values[j] << ", ";
 			}
 			cout << endl;
 		}
 	}
 }
-
-
--- mainHash.cpp
+++ mainHash.cpp
@@ -15,9 +15,9 @@ using namespace std;
 int main() {
 	srand(time(NULL));
 	makeSeuss k("DrSeuss.txt","Seussout.txt",true,true);
-	makeSeuss m("GEChap1a.txt","GEChap1out.txt",false,true);
+	// makeSeuss m("GEChap1a.txt","GEChap1out.txt",false,true);
 
-	makeSeuss n("DrSeuss.txt","Seussout2.txt",true,false);
-	makeSeuss v("GEChap1a.txt","GEChap1out2.txt",false,false);
+	// makeSeuss n("DrSeuss.txt","Seussout2.txt",true,false);
+	// makeSeuss v("GEChap1a.txt","GEChap1out2.txt",false,false);
 	return 0;
 }
